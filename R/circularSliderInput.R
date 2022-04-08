3c55c34fccb00eaaaf7fdb8f5e13ae1e5e2407a4
Federico Marotta <fmarotta@protonmail.com>
2022-04-08 14:02:13 +0200

    add css and improve js bindings

--- R/circularSliderInput.R
+++ R/circularSliderInput.R
@@ -3,19 +3,23 @@
 #' @export
 circularSliderInput <- function(inputId, label, value) {
     addResourcePath(
-        prefix = "wwwCircularSlider",
-        directoryPath = system.file("www", package = "cislShinyBindings")
-    )
-    addResourcePath(
         prefix = "vendorCircularSlider",
         directoryPath = system.file("vendor/circularSlider", package = "cislShinyBindings")
+    )
+    addResourcePath(
+        prefix = "wwwCircularSlider",
+        directoryPath = system.file("www", package = "cislShinyBindings")
     )
     tagList(
         singleton(tags$head(
             tags$script(src = "vendorCircularSlider/js/circularSlider.js"),
             tags$script(src = "wwwCircularSlider/cislShinyBindings.js"),
-            tags$link(rel = "stylesheet", type = "text/css", href = "vendorCircularSlider/css/circularSlider.css")
+            tags$link(rel = "stylesheet", type = "text/css", href = "vendorCircularSlider/css/circularSlider.css"),
+            tags$link(rel = "stylesheet", type = "text/css", href = "wwwCircularSlider/cislShinyBindings.css")
         )),
-        tags$input(type = "text", id = "my-input", class = "circularSlider", "data-height" = 600)
+        tags$input(type = "text", id = inputId, class = "CircularSlider",
+            "data-height" = 600,
+            "data-ruler-inside" = F
+        )
     )
 }
--- inst/www/cislShinyBindings.js
+++ inst/www/cislShinyBindings.js
@@ -1,25 +1,31 @@
 var cislBinding = new Shiny.InputBinding();
 
+
 $.extend(cislBinding, {
-
   find: function(scope) {
-    return $(scope).find("input.circularSlider");
+    return $(scope).find(".CircularSlider");
   },
-
   initialize: function(el){
     $(el).circularSlider();
+    $(el).data("circularSlider").$input.after(
+        '<svg width="0" height="0">' +
+        '<defs>' +
+            '<radialGradient id="cisl-shiny-gradient" r="53%">' +
+                '<stop offset="90%" stop-color="#fff" />' +
+                '<stop offset="110%" stop-color="#dedede" />' +
+            '</radialGradient>' +
+            '</defs>' +
+        '</svg>'
+    )
   },
-
   getValue: function(el) {
     return $(el).data("circularSlider").get_value_array();
   },
-
   subscribe: function(el, callback) {
       $(el).data("circularSlider").onFinish = function() {
           callback(true);
       }
   }
-
 });
 
 Shiny.inputBindings.register(cislBinding);

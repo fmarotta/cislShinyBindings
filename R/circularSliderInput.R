8cec8988eb3dd899eb4d6e92badf98e05f6e556b
Federico Marotta <fmarotta@protonmail.com>
2022-04-14 17:02:15 +0200

    customise the slider through data- props

--- R/circularSliderInput.R
+++ R/circularSliderInput.R
@@ -1,7 +1,11 @@
 #' circularSliderInput
 #'
 #' @export
-circularSliderInput <- function(inputId, label, value) {
+circularSliderInput <- function(inputId, label, min, max, value,
+                                step = NULL, breaks_n = 100, major_breaks_every = 10,
+                                ruler_inside = TRUE, breaks_altitude = 23,
+                                labels_altitude = 11, digits = 2, prefix = NULL,
+                                postfix = NULL, values_sep = ":") {
     addResourcePath(
         prefix = "vendorCircularSlider",
         directoryPath = system.file("vendor/circularSlider", package = "cislShinyBindings")
@@ -10,6 +14,10 @@ circularSliderInput <- function(inputId, label, value) {
         prefix = "wwwCircularSlider",
         directoryPath = system.file("www", package = "cislShinyBindings")
     )
+    if (ruler_inside)
+        ruler_inside <- "true"
+    else
+        ruler_inside <- "false"
     tagList(
         singleton(tags$head(
             tags$script(src = "vendorCircularSlider/js/circularSlider.js"),
@@ -17,9 +25,24 @@ circularSliderInput <- function(inputId, label, value) {
             tags$link(rel = "stylesheet", type = "text/css", href = "vendorCircularSlider/css/circularSlider.css"),
             tags$link(rel = "stylesheet", type = "text/css", href = "wwwCircularSlider/cislShinyBindings.css")
         )),
-        tags$input(type = "text", id = inputId, class = "CircularSlider",
-            "data-height" = 600,
-            "data-ruler-inside" = F
+        div(class = "form-group shiny-input-container",
+            shiny:::shinyInputLabel(inputId, label),
+            tags$input(type = "text", id = inputId, class = "CircularSlider",
+                "data-min" = min,
+                "data-max" = max,
+                "data-from" = value[1],
+                "data-to" = value[2],
+                "data-step" = step,
+                "data-breaks-n" = breaks_n,
+                "data-major-breaks-every" = major_breaks_every,
+                "data-ruler-inside" = ruler_inside,
+                "data-breaks-altitude" = breaks_altitude,
+                "data-labels-altitude" = labels_altitude,
+                "data-digits" = digits,
+                "data-prefix" = prefix,
+                "data-postfix" = postfix,
+                "data-values-sep" = values_sep
+            )
         )
     )
 }
